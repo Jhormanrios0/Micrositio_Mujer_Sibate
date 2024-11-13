d1017150ba20839bc80b8be926a1ec284c552616
jhorman <78864961+Jhormanrios0@users.noreply.github.com>
2024-11-13 17:19:59 -0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,62 +1,77 @@
-var splide = new Splide(".slider__home", {
+var splideHome = new Splide(".slider__home", {
   classes: {
     arrows: "splide__arrows translate-y-0 transform",
     arrow: "rounded-full border-[1px] border-white",
     prev: "splide__arrow--prev order-1 relative translate-y-0 transform [&>svg]:fill-white left-0 [&>svg]:w-[25px] [&>svg]:h-[25px] p-[10px]",
     next: "splide__arrow--next order-3 relative translate-y-0 transform [&>svg]:fill-white right-0 [&>svg]:w-[25px] [&>svg]:h-[25px] p-[10px]",
-    pagination: "splide__pagination ",
-    page: "splide__pagination__page border-2 border-solid  w-3 h-3  lg:w-4 lg:h-4 shadow-md ",
+    pagination: "splide__pagination",
+    page: "splide__pagination__page border-2 border-solid w-3 h-3 lg:w-4 lg:h-4 shadow-md",
   },
   rewind: true,
   autoplay: "play",
 });
-splide.mount();
+splideHome.mount();
 
-document.addEventListener("DOMContentLoaded", function () {
-  new Splide("#splide", {
-    type: "loop",
-    perPage: 3,
-    gap: "0.5rem",
-    pagination: true,
-    arrows: true,
-    autoplay: true,
-    interval: 70000,
-    breakpoints: {
-      500: {
-        perPage: 1,
-      },
-    },
-  }).mount();
+var splideImageSlider = new Splide("#image-slider", {
+  classes: {
+    pagination:
+      "splide__pagination relative mt-[20px] flex justify-center gap-[10px]",
+    page: "splide__pagination__page border-2 border-solid w-3 h-3 lg:w-4 lg:h-4 shadow-md",
+  },
+  rewind: true,
+  loop: true,
+  autoplay: true,
+  interval: 2000,
+  arrows: false,
+});
+splideImageSlider.mount();
+
+// Obtener elementos
+const datePicker = document.getElementById("date-picker");
+const dateModal = document.getElementById("date-modal");
+const dateInput = document.getElementById("date-input");
+const dateDisplay = document.getElementById("date-display");
+const closeModalButton = document.getElementById("close-modal");
+
+// Abrir el modal al hacer clic en el contenedor del ícono y la fecha
+datePicker.addEventListener("click", () => {
+  dateModal.style.display = "flex";
 });
 
-const btnSemana = document.getElementById("btnSemana");
-const btnMes = document.getElementById("btnMes");
-const apartado1 = document.getElementById("apartado1");
-const apartado2 = document.getElementById("apartado2");
+// Cerrar el modal al hacer clic en el botón de cerrar
+closeModalButton.addEventListener("click", () => {
+  closeDateModal();
+});
 
-// Función para mostrar el apartado seleccionado y activar el borde
-function mostrarApartado(apartadoAMostrar) {
-  if (apartadoAMostrar === "semana") {
-    apartado1.classList.remove("hidden");
-    apartado2.classList.add("hidden");
-    activarBorde(btnSemana);
+// Cerrar el modal y actualizar la fecha cuando se selecciona una fecha
+dateInput.addEventListener("change", () => {
+  updateDateDisplay();
+  closeDateModal();
+});
+
+// Función para cerrar el modal
+function closeDateModal() {
+  dateModal.style.display = "none";
+}
+
+// Función para actualizar la visualización de la fecha en el span
+function updateDateDisplay() {
+  if (dateInput.value) {
+    // Formatear la fecha
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    const formattedDate = new Date(dateInput.value).toLocaleDateString(
+      "es-ES",
+      options
+    );
+    dateDisplay.textContent = formattedDate;
   } else {
-    apartado2.classList.remove("hidden");
-    apartado1.classList.add("hidden");
-    activarBorde(btnMes);
+    dateDisplay.textContent = "Fecha";
   }
 }
 
-// Función para activar el borde del elemento seleccionado
-function activarBorde(elemento) {
-  btnSemana.classList.remove("border-b-4", "border-secundaryColor");
-  btnMes.classList.remove("border-b-4", "border-secundaryColor");
-  elemento.classList.add("border-b-4", "border-secundaryColor");
-}
-
-// Evento para mostrar el contenido al hacer clic
-btnSemana.addEventListener("click", () => mostrarApartado("semana"));
-btnMes.addEventListener("click", () => mostrarApartado("mes"));
-
-// Mostrar "Esta Semana" por defecto al cargar la página
-window.addEventListener("DOMContentLoaded", () => mostrarApartado("semana"));
+// Cerrar el modal al hacer clic fuera del cuadro de diálogo
+window.addEventListener("click", (event) => {
+  if (event.target === dateModal) {
+    closeDateModal();
+  }
+});
